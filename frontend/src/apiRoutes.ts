94acc8ecfa7ef931b7f2b05688fefdd3e5488f87
Weronika <wukostrze@gmail.com>
2024-12-08 00:22:12 +0100

    feat(frontend): Added weather type

--- frontend/src/apiRoutes.ts
+++ frontend/src/apiRoutes.ts
@@ -33,6 +33,17 @@ export type Place = {
   rating: number;
 }
 
+export type Weather = {
+  clouds: number;
+  visibility: number;
+  humidity: number;
+  moon: number;
+  planet: string[];
+  magnitude: number;
+  sunrise: string;
+  sunset: string;
+}
+
 export class GetMeetingsRoute extends Route {
   data: never;
   response: {
